97a9c09ac6bc50761decc84031dcd90b31f5286d
Kirill Romanov <kirill.coldline@gmail.com>
2021-04-06 20:38:53 +0300

    0. Spring MVC [#376243]

---
+++ src/main/java/ru/job4j/accident/WebInit.java
@@ -0,0 +1,21 @@
+package ru.job4j.accident;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import ru.job4j.accident.config.WebConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebInit implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.refresh();
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
