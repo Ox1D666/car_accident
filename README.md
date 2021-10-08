6c6b540ac1e7124e3dacf5d03c5047f0968416ef
Kirill Romanov <kirill.coldline@gmail.com>
2021-10-08 11:58:56 +0300

    2. IndexControl. Таблица и вид. [#376242]

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # car_accident
+`1. About project `
+
+System which handles and store information about car accidents. I will add images
+when my program has UI.
+
+`2. Configures to start`
+
+On this stage the program don't need a special configures to start.
+
+`3. How to use.`
+
+We have two roles: common users & inspectors. User add request about car-accident.
+In request user write car number, address, accident description and add a photo.
+When the request completed, inspector checks it for mistakes, and set status: Approve,
+Reject or Complete.
+
+ `4. Contact.`
+ Write me: kirill.coldline@gmail.com, if u have any questions or suggestions.
--- src/main/java/ru/job4j/accident/control/IndexController.java
+++ src/main/java/ru/job4j/accident/control/IndexController.java
@@ -3,18 +3,19 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.repository.AccidentMem;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
-    AccidentMem store = new AccidentMem();
-    List<Accident> result =  store.getAccidents();
+    private AccidentMem store;
+
+    public IndexController(AccidentMem accidentMem) {
+        store = accidentMem;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", result);
+        model.addAttribute("accidents", store.getAccidents());
         return "index";
     }
 }
